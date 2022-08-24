64d30e98b251c831ee2a6dc5b926fd115fc09d21
Vitalii Karasov <62566664+VitaliiKarasov@users.noreply.github.com>
2022-08-24 10:36:05 +0300

    Update main.js

--- HW3/main.js
+++ HW3/main.js
@@ -1,12 +1,27 @@
 //--------------------------1-----------------------
-Array.prototype.myFilter = function (callbackFn) {
+Array.prototype.myFilter = function (callback, thisArg) {
+  if (this === null) {
+    throw new Error("Can not iterate");
+  }
+  if (typeof callback !== "function") {
+    throw new Error("Callback is not a function");
+  }
+
+  let context = this;
+
+  const ObjectFromArray = Object(this);
+
+  if (arguments.length > 1) {
+    context = thisArg;
+  }
+
   const filtered = [];
 
-  for (let i = 0; i < this.length; i++) {
-    const element = callbackFn(this[i], i, this);
-
-    if (element) {
-      filtered.push(element);
+  for (let i = 0; i < ObjectFromArray.length; i++) {
+    if (i in ObjectFromArray) {
+      if (callback.call(context, context[i], i, ObjectFromArray)) {
+        filtered.push(context[i]);
+      }
     }
   }
 
